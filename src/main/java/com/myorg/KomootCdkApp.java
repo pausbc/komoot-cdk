53de56598234ac6657ba52dbe7f7e100f4710aa8
paulius.subacius@telesoftas.com <paulius.subacius@telesoftas.com>
2020-06-11 21:13:50 +0300

    Basic lambda function, ddb, SNS Topic stack

--- src/main/java/com/myorg/KomootCdkApp.java
+++ src/main/java/com/myorg/KomootCdkApp.java
@@ -1,15 +1,16 @@
 package com.myorg;
 
 import software.amazon.awscdk.core.App;
-
-import java.util.Arrays;
+import software.amazon.awscdk.core.Environment;
+import software.amazon.awscdk.core.StackProps;
 
 public class KomootCdkApp {
+
     public static void main(final String[] args) {
         App app = new App();
-
-        new KomootCdkStack(app, "KomootCdkStack");
-
+        Environment euWest1 = Environment.builder().region("eu-west-1").build();
+        new KomootCdkStack(app, "KomootCdkStack", StackProps.builder().env(euWest1).build());
         app.synth();
     }
 }
+
--- src/main/java/com/myorg/KomootCdkStack.java
+++ src/main/java/com/myorg/KomootCdkStack.java
@@ -1,17 +1,48 @@
 package com.myorg;
 
-import software.amazon.awscdk.core.Construct;
-import software.amazon.awscdk.core.Stack;
-import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.core.*;
+import software.amazon.awscdk.services.dynamodb.Attribute;
+import software.amazon.awscdk.services.dynamodb.AttributeType;
+import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.TableProps;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.FunctionProps;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.lambda.eventsources.SnsEventSource;
+import software.amazon.awscdk.services.sns.ITopic;
+import software.amazon.awscdk.services.sns.Topic;
 
 public class KomootCdkStack extends Stack {
-    public KomootCdkStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
 
     public KomootCdkStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        Attribute partitionKey = Attribute.builder()
+                .name("id")
+                .type(AttributeType.NUMBER)
+                .build();
+
+        TableProps tableProps = TableProps.builder()
+                .tableName("Users")
+                .partitionKey(partitionKey)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+
+        Table usersTable = new Table(this, "users", tableProps);
+
+        FunctionProps functionProps = FunctionProps.builder()
+                .code(Code.fromAsset("./asset/signups-lambda-1.0.jar"))
+                .handler("com.pausub.challenge.signups.SignupRequestHandler::handleRequest")
+                .runtime(Runtime.JAVA_11)
+                .timeout(Duration.minutes(2))
+                .memorySize(256)
+                .build();
+
+        Function signupHandlerLambda = new Function(this, "challenge-greet-user-test", functionProps);
+        usersTable.grantFullAccess(signupHandlerLambda);
+
+        ITopic signupTopic = Topic.fromTopicArn(this, "challenge-backend-signups", "arn:aws:sns:eu-west-1:963797398573:challenge-backend-signups");
+        signupHandlerLambda.addEventSource(new SnsEventSource(signupTopic));
     }
 }
